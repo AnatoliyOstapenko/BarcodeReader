cc035fef83a17c2393b7111b4cfcd3b6672b9a5e
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2023-01-31 20:43:12 +0200

    Initial Commit

---
+++ BarcodeReader/BarcodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeReaderApp.swift
+//  BarcodeReader
+//
+//  Created by AnatoliiOstapenko on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
